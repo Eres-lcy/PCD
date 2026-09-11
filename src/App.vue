a5fc2a6eb3a2954e90aa0d6036d02598d9b2be39
Chengyun Li <er3shkigal.lcy@gmail.com>
2026-09-11 14:41:26 +0800

    adjust author sequence

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,10 @@
         Chengyun Li<sup>1</sup>,
         Ji Zhang<sup>1</sup>,
         Xu Luo<sup>1</sup>,
+        Lianli Gao<sup>1</sup>,
         Shihan Wu<sup>2</sup>,
         Pengpeng Zeng<sup>3</sup>,
-        Jingkuan Song<sup>3</sup>,
-        and Lianli Gao<sup>1</sup>
+        and Jingkuan Song<sup>3</sup>,
       </div>
 
       <div class="institutions">
